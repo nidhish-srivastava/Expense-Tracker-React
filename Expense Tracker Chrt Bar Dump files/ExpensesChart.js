8ce7d2ca2c2b6557fba6fbddf0512f1f9322d3c1
Nidhish Srivastava <nidhish0430@gmail.com>
2023-01-25 18:32:05 +0530

    Modify these changes which make this app. lil responive

---
+++ Expense Tracker Chrt Bar Dump files/ExpensesChart.js
@@ -0,0 +1,34 @@
+import React from 'react'
+// import Chart from '../Chart/Chart'
+
+function ExpensesChart(props) {
+    
+    // We will create an array of objects
+    
+    const ChartdataPoints = [
+        { label:'Jan',value:0 },
+        { label:'Feb',value:0 },
+        { label:'Mar',value:0 },
+        { label:'Apr',value:0 },
+        { label:'May',value:0 },
+        { label:'Jun',value:0 },
+        { label:'Jul',value:0 },
+        { label:'Aug',value:0 },
+        { label:'Sept',value:0},
+        { label:'Oct',value:0 },
+        { label:'Nov',value:0 },
+        { label:'Dec',value:0 }
+    ]
+
+    for(const expense of props.expenses){
+        const expenseMonth = expense.date.getMonth();
+        ChartdataPoints[expenseMonth].value += expense.amount;
+    }
+
+  return (
+    // <Chart dataPoints = {ChartdataPoints}/>
+    <React.Fragment></React.Fragment>
+  )
+}
+
+export default ExpensesChart
--- src/Expenses/ExpenseItem.jsx
+++ src/Expenses/ExpenseItem.jsx
@@ -7,8 +7,8 @@ function ExpenseItem(props) {
   return (
       <div className='expense-item'>
       <ExpenseDate tarik={props.date} />
+      <h2>{props.title}</h2>  {/* Could have inserted h2 inside the div,but didnt coz of responsiveness */}
       <div className='expenseItem-content'>
-      <h2>{props.title}</h2>
       <i className="fa fa-trash-o" aria-hidden="true" onClick={()=> {
              props.deleteExpense(props.id) }}></i>
       <div className='expenseItem-amount'>&#8377; {props.amount}</div>
